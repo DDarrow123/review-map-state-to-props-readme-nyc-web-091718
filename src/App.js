bca6d23502d16b51e87b244db5f91be7c356474e
DDarrow123 <ddarrowphl@gmail.com>
2018-12-08 14:27:31 -0500

    built review code-along

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -34,8 +34,18 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  //this state is the ENTIRE state of the store
+  // debugger;
+  return { orangePeel: ['a', 'b', 'c']  }
 }
 
-export default connect(mapStateToProps)(App);
+// const vanilla = (milkshake) => {
+//   debugger;
+//   return { items: milkshake.items }
+// }
+
+export default connect( state => ({ items: state.items }))(App)
+
+// export default connect(vanilla)(App);
+
+// export default connect(mapStateToProps)(App);
